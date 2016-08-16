1a61103254f983406380ecf71d1c8306a6a4eab7
designatnight <designatnight1@gmail.com>
2016-08-15 22:28:11 -0700

    #2 created object model for a card and deck

---
+++ src/main/java/stevesmith/deckvendor/datatype/Suit.java
@@ -0,0 +1,22 @@
+package stevesmith.deckvendor.datatype;
+
+public enum Suit {
+
+	SPADE("S", "Spades"), HEART("H", "Hearts"), CLUB("C", "Clubs"), DIAMOND("D", "Diamonds");
+
+	private String shortName;
+	private String fullName;
+
+	private Suit(String shortName, String fullName) {
+		this.shortName = shortName;
+		this.fullName = fullName;
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+}
